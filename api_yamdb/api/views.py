40282da27b269c00404bba918b1568be1675feed
Александр Лонский <alexander.v.lonskiy@gmail.com>
2022-08-19 23:50:55 +0300

    Synchronized versions

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -3,8 +3,10 @@ from rest_framework import filters, generics, viewsets
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import AllowAny, IsAuthenticatedOrReadOnly
 
-#from .permissions import AuthorOrReadOnly
-from .serializers import UsersSerializer, ReviewsSerializer, CommentsSerializer, CategoriesSerializer, GenresSerializer, TitlesSerializer
+from .permissions import IsAdminOrReadOnly
+from .serializers import (
+    UsersSerializer, ReviewsSerializer, CommentsSerializer,
+    CategoriesSerializer, GenresSerializer, TitlesSerializer)
 
 
 class UsersViewSet(viewsets.ModelViewSet):
@@ -20,7 +22,8 @@ class ReviewsViewSet(viewsets.ModelViewSet):
 class CommentsViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentsSerializer
-    
+
+
 class CategoriesViewSet(viewsets.ModelViewSet):
     queryset = Categories.objects.all()
     serializer_class = CategoriesSerializer
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -15,13 +15,10 @@ class User(AbstractUser):
     email = models.EmailField(max_length=254, blank=False)
     bio = models.CharField(max_length=150, blank=True)
     role = models.CharField(choices=USER_STATUS, default='user', max_length=10)
-    confirmation_code = models.CharField(max_length=255, null=True, blank=False)
+    confirmation_code = models.CharField(
+        max_length=255, null=True, blank=False)
 
 
-class Category(models.Model):
-    name=models.CharField(max_length=200)
-    slug=models.SlugField(max_length=50, unique=True)
-    
 class Categories(models.Model):
     name = models.CharField(max_length=200)
     slug = models.SlugField(max_length=20, unique=True)
@@ -42,20 +39,16 @@ class Titles(models.Model):
     name = models.CharField(max_length=200)
     year = models.IntegerField()
     category = models.ForeignKey(
-        Category,
+        Categories,
         on_delete=models.SET_NULL,
         null=True,
         related_name='titles')
 
-class Genre(models.Model):
-    name=models.CharField(max_length=200)
-    slug=models.SlugField(max_length=50, unique=True)
-
 
 class Reviews(models.Model):
     title_id = models.ForeignKey(
-        Titles, 
-        on_delete=models.CASCADE, 
+        Titles,
+        on_delete=models.CASCADE,
         related_name='titles')
     text = models.TextField()
     author = models.ForeignKey(
@@ -63,13 +56,14 @@ class Reviews(models.Model):
         on_delete=models.CASCADE,
         related_name='reviews')
     pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
-    score = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(10.0)])
+    score = models.FloatField(
+        validators=[MinValueValidator(0.0), MaxValueValidator(10.0)])
 
 
 class Comment(models.Model):
     review_id = models.ForeignKey(
-        Reviews, 
-        on_delete=models.CASCADE, 
+        Reviews,
+        on_delete=models.CASCADE,
         related_name='reviews')
     text = models.TextField()
     author = models.ForeignKey(
