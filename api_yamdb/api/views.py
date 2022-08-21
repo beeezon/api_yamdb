046fc9108f9455405ae9bbf2a98832142fe2e0ff
Александр Лонский <alexander.v.lonskiy@gmail.com>
2022-08-21 10:46:06 +0300

    TitlesViewSet typo correction

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -41,7 +41,7 @@ class GenresViewSet(viewsets.ModelViewSet):
 
 
 class TitlesViewSet(viewsets.ModelViewSet):
-    quesryset = Titles.objects.all()
+    queryset = Titles.objects.all()
     serializer_class = TitlesSerializer
     permission_classes = (IsAdminOrReadOnly,)
     filter_backends = (filters.SearchFilter,)
