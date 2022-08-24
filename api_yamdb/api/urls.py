835de9869db66e6abe3f1884bdff7447ada458ac
Александр Лонский <alexander.v.lonskiy@gmail.com>
2022-08-25 00:12:42 +0300

    fixed pytests

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import (
+from rest_framework_simplejwt.views import(
     TokenObtainPairView,
     TokenRefreshView,
 )
