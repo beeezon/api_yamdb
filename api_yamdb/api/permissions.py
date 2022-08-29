a5b85cd76fd62c5c6990cda9306cc59c018714a0
Александр Лонский <alexander.v.lonskiy@gmail.com>
2022-08-29 21:33:24 +0300

    test commit, ignore pls

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -6,7 +6,9 @@ class IsAdminOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return (request.method in permissions.SAFE_METHODS
-                or request.user.is_superuser)
+                or (request.user.is_authenticated
+                    and (request.user.is_staff
+                         or request.user.is_superuser)))
 
 
 class IsAuthorAdminModerOrReadOnly(permissions.BasePermission):
